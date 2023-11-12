b5ab03c4f3fe4e3d3403f170f189ccef97aa69f4
aubbiali <aubbiali@latek.it>
2023-11-12 22:51:24 +0100

    Created database

---
+++ src/main/resources/db_changelog/01-pizza.sql
@@ -0,0 +1,21 @@
+-- liquibase formatted sql
+
+-- changeset liquibase aubbiali:1
+CREATE TABLE awesomepizza.pizza (
+    id INT,
+    name VARCHAR(50) NOT NULL,
+    cost INT CONSTRAINT cost CHECK (cost > 0) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE awesomepizza.ingredients (
+    id INT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE awesomepizza.pizza_composition (
+    id_pizza INT,
+    id_ingredient INT,
+    FOREIGN KEY (id_pizza) REFERENCES pizza(id) ON DELETE no action ON UPDATE no action,
+    FOREIGN KEY (id_ingredient) REFERENCES ingredients(id) ON DELETE no action ON UPDATE no action,
+    PRIMARY KEY(id_pizza, id_ingredient)
+);
---
+++ src/main/resources/db_changelog/02-order.sql
@@ -0,0 +1,32 @@
+-- liquibase formatted sql
+
+-- changeset liquibase aubbiali:2
+CREATE TABLE awesomepizza.orders (
+    id INT,
+    insert_date DATE NOT NULL,
+    order_date DATE NOT NULL,
+    slot INT CONSTRAINT slot CHECK (slot >= 1 AND slot <= 2) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE awesomepizza.status(
+    id INT,
+    status VARCHAR(25) NOT NULL,
+    PRIMARY KEY (id)
+);
+CREATE TABLE awesomepizza.order_status (
+    id_order INT,
+    id_status INT,
+    change_date Date NOT NULL,
+    PRIMARY KEY (id_order, id_status),
+    FOREIGN KEY (id_order) REFERENCES orders(id) ON DELETE no action ON UPDATE no action,
+    FOREIGN KEY (id_status) REFERENCES status(id) ON DELETE no action ON UPDATE no action
+);
+CREATE TABLE awesomepizza.order_pizza (
+    id_order INT,
+    id_pizza INT,
+    quantity INT CONSTRAINT quantity CHECK (quantity > 0) NOT NULL,
+    Changes VARCHAR,
+    PRIMARY KEY (id_order, id_pizza),
+    FOREIGN KEY (id_order) REFERENCES orders(id) ON DELETE no action ON UPDATE no action,
+    FOREIGN KEY (id_pizza) REFERENCES pizza(id) ON DELETE no action ON UPDATE no action
+);
