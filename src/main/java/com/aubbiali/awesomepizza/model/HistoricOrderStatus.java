33f0c2c0634d22b285c9fe63c366015055b7d97c
aubbiali <aubbiali@latek.it>
2023-11-14 18:14:37 +0100

    Added historic order status entity

---
+++ src/main/java/com/aubbiali/awesomepizza/model/HistoricOrderStatus.java
@@ -0,0 +1,28 @@
+package com.aubbiali.awesomepizza.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "HistoricOrderStatus", schema = "awesomepizza")
+@Setter @Getter
+public class HistoricOrderStatus {
+
+    @ManyToOne
+    @JoinColumn(name = "id_order")
+    @Id
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "id_status")
+    @Id
+    private Status status;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    private Date changeDate;
+}
