4a5b33bf4863e1b2f39aa388f696ce78b380db4d
aubbiali <aubbiali@latek.it>
2023-11-15 00:31:45 +0100

    Updated global exception handler

--- src/main/java/com/aubbiali/awesomepizza/controller/GlobalExceptionHandler.java
+++ src/main/java/com/aubbiali/awesomepizza/controller/GlobalExceptionHandler.java
@@ -1,7 +1,11 @@
 package com.aubbiali.awesomepizza.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,11 +15,29 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Object> handleException(Exception ex) {
+        logger.error("Exception occurred:", ex);
+
+
         Map<String, Object> body = new HashMap<>();
         body.put("message", "SOMETHING WRONG! exception occurred " + ex);
 
         return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        return  new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
 }
