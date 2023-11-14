96f599f2ca0b0872824af0f8abaf9735a12fd9ad
aubbiali <aubbiali@latek.it>
2023-11-14 16:47:56 +0100

    Fix ingredient entity

--- src/main/java/com/aubbiali/awesomepizza/model/Ingredient.java
+++ src/main/java/com/aubbiali/awesomepizza/model/Ingredient.java
@@ -1,9 +1,6 @@
 package com.aubbiali.awesomepizza.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,7 +11,7 @@ import lombok.Setter;
 public class Ingredient {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @NotNull
