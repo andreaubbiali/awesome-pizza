ed8f33e1bf811e0522cb962cbbf8766c4886d246
aubbiali <aubbiali@latek.it>
2023-11-16 10:40:35 +0100

    Added enable scheduling

--- src/main/java/com/aubbiali/awesomepizza/AwesomePizzaApplication.java
+++ src/main/java/com/aubbiali/awesomepizza/AwesomePizzaApplication.java
@@ -2,8 +2,10 @@ package com.aubbiali.awesomepizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AwesomePizzaApplication {
 
 	public static void main(String[] args) {
