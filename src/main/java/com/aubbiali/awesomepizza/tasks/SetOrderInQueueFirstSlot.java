e4945aae7ca6b3649d5f91f31a07569e4ad51fb2
aubbiali <aubbiali@latek.it>
2023-11-16 10:40:15 +0100

    Added scheduled job

---
+++ src/main/java/com/aubbiali/awesomepizza/tasks/SetOrderInQueueFirstSlot.java
@@ -0,0 +1,19 @@
+package com.aubbiali.awesomepizza.tasks;
+
+import com.aubbiali.awesomepizza.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SetOrderInQueueFirstSlot {
+
+    @Autowired
+    OrderService orderService;
+
+    @Scheduled(cron = "0 0 12 * * * *")
+    public void setOrderInQueue(){
+        orderService.setOrderOfTodayInQueue(1);
+    }
+
+}
