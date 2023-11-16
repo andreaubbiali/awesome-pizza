ee5720a555c9169e1bccb4e951c34eb9276e533b
aubbiali <aubbiali@latek.it>
2023-11-16 10:15:23 +0100

    Added status enum methods

--- src/main/java/com/aubbiali/awesomepizza/model/StatusEnum.java
+++ src/main/java/com/aubbiali/awesomepizza/model/StatusEnum.java
@@ -1,9 +1,25 @@
 package com.aubbiali.awesomepizza.model;
 
+import com.aubbiali.awesomepizza.model.exceptions.EndStatusException;
+
 public enum StatusEnum {
     CREATED,
     IN_QUEUE, // todo add daily job to update status in queue for all orders of the day.
     IN_PROGRESS,
     READY_FOR_PICKUP,
-    COMPLETED
+    COMPLETED;
+
+    public StatusEnum getNextStatus(StatusEnum previous) throws EndStatusException{
+        return switch (previous){
+            case CREATED -> IN_QUEUE;
+            case IN_QUEUE -> IN_PROGRESS;
+            case IN_PROGRESS -> READY_FOR_PICKUP;
+            case READY_FOR_PICKUP -> COMPLETED;
+            case COMPLETED -> throw new EndStatusException();
+        };
+    }
+
+    public StatusEnum getEndStatus(){
+        return COMPLETED;
+    }
 }
---
+++ src/main/java/com/aubbiali/awesomepizza/model/exceptions/EndStatusException.java
@@ -0,0 +1,4 @@
+package com.aubbiali.awesomepizza.model.exceptions;
+
+public class EndStatusException extends Exception{
+}
